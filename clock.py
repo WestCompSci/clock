5fa23df163f683ba1ebfb439322db3b81589f81a
Jonathan Nicholls <jonathanrnicholls@gmail.com>
2018-05-11 18:56:16 +0800

    Put everything into a function and added user input
    I've put all of the print functions under a single function, display, and also added user input.

--- clock.py
+++ clock.py
@@ -17,39 +17,43 @@ num_ref = { # This is a reference dictionary where numbers, like 8, are connecte
     9 : [1, 1, 1, 1, 0, 0, 1],
     0 : [1, 1, 0, 1, 1, 1, 1]
 }
-register = num_ref[4]
 
-if register[0] == 1:
-    print (" __")
-else:
-    print ("   ")
+def display (num):
+    register = num_ref[num]
+    if register[0] == 1:
+        print(" __")
+    else:
+        print("   ")
 
-if register[1] == 1:
-    print ("|", end="")  # This ensures that the below will not be on a new line
-else:
-    print (" ", end="")
+    if register[1] == 1:
+        print("|", end="")  # This ensures that the below will not be on a new line
+    else:
+        print(" ", end="")
 
-if register[2] == 1:
-    print ("__", end="")
-else:
-    print ("  ", end="")
+    if register[2] == 1:
+        print("__", end="")
+    else:
+        print("  ", end="")
 
-if register[3] == 1:
-    print ("|")
-else:
-    print (" ")
+    if register[3] == 1:
+        print("|")
+    else:
+        print(" ")
 
-if register[4] == 1:
-    print ("|", end="")
-else:
-    print (" ", end="")
+    if register[4] == 1:
+        print("|", end="")
+    else:
+        print(" ", end="")
 
-if register[5] == 1:
-    print ("__", end="")
-else:
-    print ("  ", end="")
+    if register[5] == 1:
+        print("__", end="")
+    else:
+        print("  ", end="")
 
-if register[6] == 1:
-    print ("|")
-else:
-    print (" ")
+    if register[6] == 1:
+        print("|")
+    else:
+        print(" ")
+
+user_in = int(input("Enter a number"))
+display(user_in)
